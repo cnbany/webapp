363e39b9d24db00917446c5522d2b959b098285b
cnbany <cnbany@163.com>
2019-08-21 11:33:50 +0800

    modi webpack config file

--- babel.config.js
+++ babel.config.js
@@ -2,11 +2,14 @@ module.exports = {
     presets: [
         ['@babel/preset-env', {
             useBuiltIns: 'usage',
+            "corejs": 3,
             targets: '> 0.25%, not dead' // Support for lower than android 6
         }]
     ],
     plugins: [
-        '@babel/plugin-transform-runtime',
+        ["@babel/plugin-transform-runtime", {
+            "corejs": 3
+        }],
         ['import', {
             libraryName: 'vant',
             libraryDirectory: 'es',
--- webpack.base.js
+++ webpack.base.js
@@ -2,55 +2,61 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const CopyWebpackPlugin = require('copy-webpack-plugin')
 const resolve = require('path').resolve
 
+
 module.exports = {
-  output: {
-    path: resolve(__dirname, 'dist'),
-    filename: './js/[name].js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        loader: 'file-loader'
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        options: {
-          cacheDirectory: true
-        },
-        exclude: file => (
-          /node_modules/.test(file) &&
-          !/\.vue\.js/.test(file)
-        )
-      }
-    ]
-  },
-  plugins: [
-    new VueLoaderPlugin(),
-    new CopyWebpackPlugin([
-      // './src/templates/api.js',
-      { from: './src/templates/fastclick.min.js', to: './js' }
-  ]),
-  ],
-  resolve: {
-    alias: {
-      '@': resolve('./src')
+    output: {
+        path: resolve(__dirname, 'dist'),
+        filename: './js/[name].js'
+    },
+    module: {
+        rules: [{
+            test: /\.vue$/,
+            loader: 'vue-loader'
+        }, { // 命中 SCSS 文件
+            test: /\.scss$/,
+            // 使用一组 Loader 去处理 SCSS 文件。
+            // 处理顺序为从后到前，即先交给 sass-loader 处理，再把结果交给 css-loader 最后再给 style-loader。
+            use: ['style-loader', 'css-loader', 'sass-loader'],
+            // 排除 node_modules 目录下的文件
+            exclude: resolve(__dirname, 'node_modules'),
+        }, {
+            test: /\.css$/,
+            use: [
+                'style-loader',
+                'css-loader'
+            ]
+        }, {
+            test: /\.(png|svg|jpg|jpeg|gif)$/,
+            loader: 'file-loader'
+        }, {
+            test: /\.(woff|woff2|eot|ttf|otf)$/,
+            loader: 'file-loader'
+        }, { // 命中 JavaScript 文件
+            test: /\.js$/,
+            // 用 babel-loader 转换 JavaScript 文件
+            // ?cacheDirectory 表示传给 babel-loader 的参数，用于缓存 babel 编译结果加快重新编译速度
+            use: ['babel-loader?cacheDirectory'],
+            // 只命中src目录里的js文件，加快 Webpack 搜索速度
+            include: resolve(__dirname, 'src'),
+            exclude: file => (
+                /node_modules/.test(file) &&
+                !/\.vue\.js/.test(file)
+            )
+        }]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new CopyWebpackPlugin([
+            // './src/templates/api.js',
+            {
+                from: './src/templates/fastclick.min.js',
+                to: './js'
+            }
+        ]),
+    ],
+    resolve: {
+        alias: {
+            '@': resolve('./src')
+        }
     }
-  }
 }
